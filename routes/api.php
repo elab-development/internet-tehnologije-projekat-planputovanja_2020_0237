8224acd8d29459f20e816d799ede32bb33b01bba
unknown <polexa01@gmail.com>
2024-01-18 23:15:51 +0100

    AuthController, login logout register opcije

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -25,14 +27,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{user_id}', [UserController::class, 'show']);
-Route::post('/users/store', [UserController::class, 'store']);
-Route::put('/users/{id}/update', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
+//Route::post('/users/store', [UserController::class, 'store']);
+//Route::put('/users/{id}/update', [UserController::class, 'update']);
+//Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
 
-
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 //Route::resource('users', UserController::class);
 //Route::resource('destinacije', DestinacijaController::class);
 //Route::resource('znamenitosti', ZnamenitostController::class);
 //Route::resource('hoteli', HotelController::class);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+   
+   
+    Route::resource('/planPutovanjas', PlanPutovanjaController::class)->only(['update','store','destroy']);
+
+    // API route for logout user
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
