b935eabf4bcf54c4095583ee3c4b970ef6367483
unknown <polexa01@gmail.com>
2024-02-08 21:26:04 +0100

    dodat web servis za najnovije vesti

--- src/components/WeatherApi.jsx
+++ src/components/WeatherApi.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import "../css/WeatherApi.css";
 import useWeatherSearch from "./useWeatherSearch"; 
+import axios from "axios";
 
 function WeatherApi() {
   const { search, setSearch, weather, setWeather, searchWeather } = useWeatherSearch();
+  const [news, setNews] = useState([]);
+  
 
+  const fetchNews = async () => {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=64086c1931554a36b9b4438d97043fcb`);
+      return response.data.articles;
+    } catch (error) {
+      console.error('Greška pri dohvatanju vesti:', error);
+      return [];
+    }
+  };
+  
   const searchPressed = async () => {
     try {
       await searchWeather();
+      const news = await fetchNews();
+      setNews(news);
     } catch (error) {
-      console.error("Greška pri pretrazi vremena:", error);
+      console.error('Greška pri pretrazi vremena:', error);
     }
   };
 
@@ -39,6 +54,7 @@ function WeatherApi() {
         </div>
 
         <WeatherDisplay weather={weather} />
+        <NewsDisplay news={news} />
       </header>
     </div>
   );
@@ -56,6 +72,26 @@ const WeatherDisplay = ({ weather }) => {
       ) : (
         ""
       )}
+    </>
+  );
+};
+
+const NewsDisplay = ({ news }) => {
+  const maxNews = 6;
+  return (
+    <>
+      {news && news.length > 0 ? (
+        <div className="News-display">
+          <h3>Najnovije vesti</h3>
+          <ul>
+            {news.slice(0, maxNews).map((article, index) => (
+              <li key={index}>
+                <a href={article.url} target="_blank" rel="noopener noreferrer">{article.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
     </>
   );
 };
